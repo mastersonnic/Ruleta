be783305431b08c199494e7425f741c4bb6996e5
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-06-06 20:17:47 -0400

    Script.js
    Script

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const spinButton = document.getElementById('spinButton');
     const confettiCanvas = document.getElementById('confetti-canvas');
     const confetti = window.confetti.create(confettiCanvas, { resize: true });
+    let isSpinning = false;
 
     spinButton.addEventListener('click', () => {
-        let newAngle = Math.floor(3600 + Math.random() * 360);
-        roulette.style.transition = 'transform 4s ease-out';
-        roulette.style.transform = `translate(-50%, -50%) rotate(${newAngle}deg)`;
+        if (!isSpinning) {
+            isSpinning = true;
+            let newAngle = Math.floor(3600 + Math.random() * 360);
+            roulette.style.transition = 'transform 4s ease-out';
+            roulette.style.transform = `translate(-50%, -50%) rotate(${newAngle}deg)`;
 
-        setTimeout(() => {
-            confettiCanvas.style.display = 'block';
-            confetti({
-                particleCount: 100,
-                spread: 70,
-                origin: { y: 0.6 }
-            });
-        }, 4000); // Se activa el confeti después de que la ruleta termine de girar
+            setTimeout(() => {
+                confettiCanvas.style.display = 'block';
+                confetti({
+                    particleCount: 100,
+                    spread: 70,
+                    origin: { y: 0.6 }
+                });
+                isSpinning = false;
+            }, 4000); // Se activa el confeti después de que la ruleta termine de girar
+        }
     });
 });
