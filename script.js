2c685933a12af15b52dc5eb2f52d1955846a977b
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-06-08 19:07:14 -0400

    Script.js
    Js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Definimos los ángulos de inicio de cada segmento en sentido horario y comenzando desde el ángulo 0 en la parte superior
-  var angulos = [0, 45, 90, 135, 180, 225, 270, 315];
-  var segmentos = ['0X', '2X', '0.5X', '4X', '0.1X', '0.02X', '6X', '1X'];
+  var segmentos = ['0X', '1X', '6X', '0.02X', '0.1X', '4X', '0.5X', '2X'];
   var imgRuleta = document.getElementById('imgRuleta');
   var resultado = document.getElementById('resultado');
   var botonGirar = document.getElementById('girar');
 
   botonGirar.addEventListener('click', function () {
-    var tiempoInicial = 3; // Tiempo en segundos que la ruleta gira a velocidad constante
-    var duracionGiro = tiempoInicial + Math.random() * 2; // Duración total del giro
-    var vueltas = 10; // Número de vueltas completas que da la ruleta antes de detenerse
+    var duracionGiro = 2; // Duración del giro en segundos
+    var gradosPorSegmento = 360 / segmentos.length;
+    var anguloAleatorio = Math.random() * gradosPorSegmento;
+    var segmentoAleatorio = Math.floor(Math.random() * segmentos.length);
+    var anguloFinal = 360 * 5 + segmentoAleatorio * gradosPorSegmento + anguloAleatorio;
 
-    // Calculamos un ángulo aleatorio entre 0 y 360 grados que corresponde al ángulo de inicio del segmento ganador
-    var anguloGanador = Math.floor(Math.random() * 8) * 45;
-    var anguloFinal = 360 * vueltas + anguloGanador;
-
-    // Actualizamos la transformación de la imagen de la ruleta para que gire
-    imgRuleta.style.transition = 'transform ' + duracionGiro + 's ease-out';
+    imgRuleta.style.transition = 'transform ' + duracionGiro + 's linear';
     imgRuleta.style.transform = 'rotate(' + anguloFinal + 'deg)';
 
-    // Mostramos el resultado una vez que la ruleta se detenga
     setTimeout(function () {
-      var segmentoGanador = segmentos[anguloGanador / 45];
+      var segmentoGanador = segmentos[segmentoAleatorio];
       resultado.textContent = 'Segmento ganador: ' + segmentoGanador;
     }, duracionGiro * 1000);
   });
