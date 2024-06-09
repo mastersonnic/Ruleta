a65e4798d5290dd358528eb5b51c32ba1f30a3cb
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-06-09 16:12:10 -0400

    script.js
    Js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
   var segmentos = ['0X', '2X', '0.5X', '4X', '0.1X', '0.02X', '6X', '1X'];
   var imgRuleta = document.getElementById('imgRuleta');
-  var mensajeGanador = document.getElementById('mensajeGanador');
+  var resultado = document.getElementById('resultado');
   var botonGirar = document.getElementById('girar');
-  var anguloActual = 0;
+  var girando = false;
 
   botonGirar.addEventListener('click', function () {
-    var duracionGiro = 2;
+    if (girando) return;
+    girando = true;
+    var duracionGiro = Math.floor(Math.random() * 3) + 2; // Duración del giro entre 2 y 4 segundos
     var gradosPorSegmento = 360 / segmentos.length;
-    var anguloInicio = 337.5;
-    var anguloFinal = anguloActual - (360 * 5 + anguloInicio);
+    var anguloInicio = 337.5; // Ángulo de inicio del primer segmento
+    var anguloAleatorio = Math.random() * gradosPorSegmento;
+    var segmentoAleatorio = Math.floor(Math.random() * segmentos.length);
+    var anguloFinal = 360 * 10 + anguloInicio + segmentoAleatorio * gradosPorSegmento + anguloAleatorio;
 
-    imgRuleta.style.transition = 'transform ' + duracionGiro + 's linear';
+    imgRuleta.style.transition = 'transform ' + duracionGiro + 's cubic-bezier(0.33, 1, 0.68, 1)';
     imgRuleta.style.transform = 'rotate(' + anguloFinal + 'deg)';
-    anguloActual = anguloFinal;
 
     setTimeout(function () {
-      var segmentoGanadorIndex = Math.floor(((360 - (anguloActual % 360)) + anguloInicio) / gradosPorSegmento) % segmentos.length;
+      var segmentoGanadorIndex = Math.floor(((anguloFinal - anguloInicio) % 360) / gradosPorSegmento);
       var segmentoGanador = segmentos[segmentoGanadorIndex];
-      mensajeGanador.textContent = '¡Haz ganado ' + segmentoGanador + '!';
-      mensajeGanador.style.display = 'block';
-      mensajeGanador.style.animation = 'aparecer 1s forwards';
+      resultado.textContent = '¡Haz ganado ' + segmentoGanador + '!';
+      resultado.style.display = 'block';
+      girando = false;
     }, duracionGiro * 1000);
   });
 });
