0dd37232270268f7fbdf527211eca875e6bc3195
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-06-08 21:26:43 -0400

    script.js
    Js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   var segmentos = ['0X', '2X', '0.5X', '4X', '0.1X', '0.02X', '6X', '1X'];
   var imgRuleta = document.getElementById('imgRuleta');
-  var resultado = document.getElementById('resultado');
+  var mensajeGanador = document.getElementById('mensajeGanador');
   var botonGirar = document.getElementById('girar');
 
   botonGirar.addEventListener('click', function () {
     var duracionGiro = 2; // Duración del giro en segundos
     var gradosPorSegmento = 360 / segmentos.length;
     var anguloInicio = 337.5; // Ángulo de inicio del primer segmento
-    var anguloAleatorio = Math.random() * gradosPorSegmento;
-    var segmentoAleatorio = Math.floor(Math.random() * segmentos.length);
-    var anguloFinal = 360 * 5 + anguloInicio + segmentoAleatorio * gradosPorSegmento + anguloAleatorio;
+    var anguloFinal = 360 * 5 + anguloInicio;
 
     imgRuleta.style.transition = 'transform ' + duracionGiro + 's linear';
     imgRuleta.style.transform = 'rotate(' + anguloFinal + 'deg)';
@@ -18,7 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
       var segmentoGanadorIndex = Math.floor(((anguloFinal - anguloInicio) % 360) / gradosPorSegmento);
       var segmentoGanador = segmentos[segmentoGanadorIndex];
-      resultado.textContent = 'Segmento ganador: ' + segmentoGanador;
+      mensajeGanador.textContent = '¡Haz ganado ' + segmentoGanador + '!';
+      mensajeGanador.style.display = 'block';
+      mensajeGanador.style.animation = 'aparecer 1s forwards';
     }, duracionGiro * 1000);
   });
 });
