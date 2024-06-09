d60e9b76402000bcd0a73deb717395c40af0d826
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-06-08 22:01:40 -0400

    script.js
    Js

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
   var imgRuleta = document.getElementById('imgRuleta');
   var mensajeGanador = document.getElementById('mensajeGanador');
   var botonGirar = document.getElementById('girar');
+  var anguloActual = 0; // Almacena el ángulo actual de la ruleta
 
   botonGirar.addEventListener('click', function () {
     var duracionGiro = 2; // Duración del giro en segundos
     var gradosPorSegmento = 360 / segmentos.length;
     var anguloInicio = 337.5; // Ángulo de inicio del primer segmento
-    var anguloFinal = -360 * 5 - anguloInicio; // Asegura que la ruleta gire hacia la izquierda
+    var anguloFinal = anguloActual - (360 * 5 + anguloInicio); // Asegura que la ruleta gire hacia la izquierda
 
     imgRuleta.style.transition = 'transform ' + duracionGiro + 's linear';
     imgRuleta.style.transform = 'rotate(' + anguloFinal + 'deg)';
+    anguloActual = anguloFinal; // Actualiza el ángulo actual para el próximo giro
 
     setTimeout(function () {
-      var segmentoGanadorIndex = Math.floor(((anguloFinal + anguloInicio) % 360) / gradosPorSegmento);
+      var segmentoGanadorIndex = Math.floor(((360 - (anguloActual % 360)) + anguloInicio) / gradosPorSegmento) % segmentos.length;
       var segmentoGanador = segmentos[segmentoGanadorIndex];
       mensajeGanador.textContent = '¡Haz ganado ' + segmentoGanador + '!';
       mensajeGanador.style.display = 'block';
