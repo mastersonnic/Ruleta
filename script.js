0b44add749b360c9db3319ae8e4a1a5b56446a43
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-07-07 14:13:07 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const ruletaVideo = document.getElementById('ruleta-video');
     const girarBtn = document.getElementById('girar-btn');
 
-    // Cambiar el video después de 1.5 segundos
-    setTimeout(() => {
-        ruletaVideo.src = 'https://raw.githubusercontent.com/mastersonnic/Ruleta/main/6X%20izq%20lento.mp4';
-    }, 1500);
+    // Cambiar el video después de 1.5 segundos al hacer clic en el botón
+    girarBtn.addEventListener('click', function () {
+        // Detener el video actual
+        ruletaVideo.pause();
+        ruletaVideo.currentTime = 0;
+
+        // Cambiar al nuevo video después de 1.5 segundos
+        setTimeout(() => {
+            ruletaVideo.src = 'https://raw.githubusercontent.com/mastersonnic/Ruleta/main/6X%20izq%20lento.mp4';
+            ruletaVideo.play();
+        }, 1500);
+    });
 });
