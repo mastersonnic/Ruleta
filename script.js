44f11bcd7fbec82f696bfdf986530179d2915648
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-26 09:41:17 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 window.onload = function() {
     fetch('https://crypto-store.cc/?locale=en&cur_from=LTC&cur_to=BCH')
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok. Status: ${response.status}`);
+            }
+            return response.text();
+        })
         .then(html => {
             const parser = new DOMParser();
             const doc = parser.parseFromString(html, 'text/html');
-            // Cambia el selector si es necesario para ajustar a la estructura actual
             const exchangeRateElement = doc.querySelector('.ng-tns-c3154908617-11.ng-star-inserted');
             if (exchangeRateElement) {
                 const exchangeRate = exchangeRateElement.textContent.trim();
                 document.getElementById('exchange-rate').textContent = `Exchange rate: ${exchangeRate}`;
             } else {
-                document.getElementById('exchange-rate').textContent = 'Exchange rate not found.';
+                document.getElementById('exchange-rate').textContent = 'Exchange rate element not found.';
             }
         })
         .catch(error => {
